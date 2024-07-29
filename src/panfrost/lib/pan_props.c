985b02bd7c6f46967ed9c66f5221695867327d5a
wesley <wesleyvanneck1012@gmail.com>
2024-07-29 18:20:06 +0200

    Revert "unknow will spoof to g610"
    This reverts commit 692fbf04cd10f7a55bb2b8c300edae05c9c6f49c.

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -78,7 +78,12 @@ const struct panfrost_model panfrost_model_list[] = {
 };
 
 const struct panfrost_model panfrost_unknown_model = {
-       MODEL(0xa867, "G610", "LODx", HAS_ANISO, 65536, {}),
+   .gpu_id = 0,
+   .name = "Unknowm Mali device (Panfrost)",
+   .performance_counters = "AAAA",
+   .min_rev_anisotropic = NO_ANISO, 
+   .tilebuffer_size = 8192, 
+   .quirks = {}, 
 };
 
 #undef NO_ANISO
